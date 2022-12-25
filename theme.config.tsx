933d43fe744f65b02a86a2159d7e1db489259ef3
meowyx <qafka256@gmail.com>
2022-12-25 06:42:04 +0600

    edited mdx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>🐱 Academy</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+
+  docsRepositoryBase: 'https://github.com/meowyx/academy-lessons',
+ 
 }
 
 export default config
